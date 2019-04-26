20659ce3dffead1fe2d4bb2b1791874dfdb0036f
Uvas12 <49845382+Uvas12@users.noreply.github.com>
2019-04-26 17:38:18 -0500

    Add files via upload

---
+++ Chemicalsources.alias.php
@@ -0,0 +1,224 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = array();
+
+/** English (English) */
+$specialPageAliases['en'] = array(
+	'ChemicalSources' => array( 'ChemicalSources' ),
+);
+
+/** Arabic (العربية) */
+$specialPageAliases['ar'] = array(
+	'ChemicalSources' => array( 'مصادر_كيميائية' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$specialPageAliases['arz'] = array(
+	'ChemicalSources' => array( 'مصادر_كيماويه' ),
+);
+
+/** Assamese (অসমীয়া) */
+$specialPageAliases['as'] = array(
+	'ChemicalSources' => array( 'ৰাসায়নিক_উৎসসমূহ' ),
+);
+
+/** Breton (brezhoneg) */
+$specialPageAliases['br'] = array(
+	'ChemicalSources' => array( 'MammennoùKimiek' ),
+);
+
+/** Bosnian (bosanski) */
+$specialPageAliases['bs'] = array(
+	'ChemicalSources' => array( 'HemijskiIzvori' ),
+);
+
+/** German (Deutsch) */
+$specialPageAliases['de'] = array(
+	'ChemicalSources' => array( 'Chemische_Quellen' ),
+);
+
+/** Zazaki (Zazaki) */
+$specialPageAliases['diq'] = array(
+	'ChemicalSources' => array( 'KimyayinÇımey' ),
+);
+
+/** Lower Sorbian (dolnoserbski) */
+$specialPageAliases['dsb'] = array(
+	'ChemicalSources' => array( 'Chemiske_žrědła' ),
+);
+
+/** Esperanto (Esperanto) */
+$specialPageAliases['eo'] = array(
+	'ChemicalSources' => array( 'Kemiaj_fontoj' ),
+);
+
+/** Spanish (español) */
+$specialPageAliases['es'] = array(
+	'ChemicalSources' => array( 'Fuentes_para_Química' ),
+);
+
+/** Persian (فارسی) */
+$specialPageAliases['fa'] = array(
+	'ChemicalSources' => array( 'منابع_شیمیایی' ),
+);
+
+/** Galician (galego) */
+$specialPageAliases['gl'] = array(
+	'ChemicalSources' => array( 'Fontes_químicas' ),
+);
+
+/** Gujarati (ગુજરાતી) */
+$specialPageAliases['gu'] = array(
+	'ChemicalSources' => array( 'રસાયણસ્રોત' ),
+);
+
+/** Upper Sorbian (hornjoserbsce) */
+$specialPageAliases['hsb'] = array(
+	'ChemicalSources' => array( 'Chemiske_žórła' ),
+);
+
+/** Haitian (Kreyòl ayisyen) */
+$specialPageAliases['ht'] = array(
+	'ChemicalSources' => array( 'SousChimik' ),
+);
+
+/** Hungarian (magyar) */
+$specialPageAliases['hu'] = array(
+	'ChemicalSources' => array( 'Vegyszer_források' ),
+);
+
+/** Interlingua (interlingua) */
+$specialPageAliases['ia'] = array(
+	'ChemicalSources' => array( 'Fontes_chimic' ),
+);
+
+/** Indonesian (Bahasa Indonesia) */
+$specialPageAliases['id'] = array(
+	'ChemicalSources' => array( 'Rujukan_kimia', 'RujukanKimia' ),
+);
+
+/** Japanese (日本語) */
+$specialPageAliases['ja'] = array(
+	'ChemicalSources' => array( '化学関連の検索' ),
+);
+
+/** Khmer (ភាសាខ្មែរ) */
+$specialPageAliases['km'] = array(
+	'ChemicalSources' => array( 'ប្រភពគីមី' ),
+);
+
+/** Korean (한국어) */
+$specialPageAliases['ko'] = array(
+	'ChemicalSources' => array( '화학자료' ),
+);
+
+/** Colognian (Ripoarisch) */
+$specialPageAliases['ksh'] = array(
+	'ChemicalSources' => array( 'Schimisch_Quelle' ),
+);
+
+/** Ladino (Ladino) */
+$specialPageAliases['lad'] = array(
+	'ChemicalSources' => array( 'ManaderosQuímicos' ),
+);
+
+/** Luxembourgish (Lëtzebuergesch) */
+$specialPageAliases['lb'] = array(
+	'ChemicalSources' => array( 'Chemesch_Quellen' ),
+);
+
+/** Macedonian (македонски) */
+$specialPageAliases['mk'] = array(
+	'ChemicalSources' => array( 'ХемискиИзвори' ),
+);
+
+/** Malayalam (മലയാളം) */
+$specialPageAliases['ml'] = array(
+	'ChemicalSources' => array( 'രസതന്ത്രസ്രോതസ്സുകൾ' ),
+);
+
+/** Marathi (मराठी) */
+$specialPageAliases['mr'] = array(
+	'ChemicalSources' => array( 'रासायनीकस्रोत' ),
+);
+
+/** Maltese (Malti) */
+$specialPageAliases['mt'] = array(
+	'ChemicalSources' => array( 'SorsiKimiċi' ),
+);
+
+/** Norwegian Bokmål (norsk (bokmål)‎) */
+$specialPageAliases['nb'] = array(
+	'ChemicalSources' => array( 'Kjemiske_kilder' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$specialPageAliases['nds-nl'] = array(
+	'ChemicalSources' => array( 'Scheikundige_bronnen' ),
+);
+
+/** Dutch (Nederlands) */
+$specialPageAliases['nl'] = array(
+	'ChemicalSources' => array( 'ScheikundigeBronnen' ),
+);
+
+/** Polish (polski) */
+$specialPageAliases['pl'] = array(
+	'ChemicalSources' => array( 'Źródła_chemiczne' ),
+);
+
+/** Pashto (پښتو) */
+$specialPageAliases['ps'] = array(
+	'ChemicalSources' => array( 'کيميايي_سرچينې' ),
+);
+
+/** Sanskrit (संस्कृतम्) */
+$specialPageAliases['sa'] = array(
+	'ChemicalSources' => array( 'रसायनस्रोत' ),
+);
+
+/** Slovak (slovenčina) */
+$specialPageAliases['sk'] = array(
+	'ChemicalSources' => array( 'ChemickéZdroje' ),
+);
+
+/** Albanian (shqip) */
+$specialPageAliases['sq'] = array(
+	'ChemicalSources' => array( 'BurimeKimike' ),
+);
+
+/** Tagalog (Tagalog) */
+$specialPageAliases['tl'] = array(
+	'ChemicalSources' => array( 'Mga_pinagmulang_makakimika' ),
+);
+
+/** Turkish (Türkçe) */
+$specialPageAliases['tr'] = array(
+	'ChemicalSources' => array( 'KimyasalKaynaklar' ),
+);
+
+/** Veps (vepsän kel’) */
+$specialPageAliases['vep'] = array(
+	'ChemicalSources' => array( 'HimižedPurtked' ),
+);
+
+/** Vietnamese (Tiếng Việt) */
+$specialPageAliases['vi'] = array(
+	'ChemicalSources' => array( 'Nguồn_hóa_học' ),
+);
+
+/** Simplified Chinese (中文（简体）‎) */
+$specialPageAliases['zh-hans'] = array(
+	'ChemicalSources' => array( '化学源' ),
+);
+
+/** Traditional Chinese (中文（繁體）‎) */
+$specialPageAliases['zh-hant'] = array(
+	'ChemicalSources' => array( '化學源' ),
+);
